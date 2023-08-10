0af62093eedeff5d2d267f4d2bb4ab504c52e9ee
Dharmarj-J <dharmaraj200303@gmail.com>
2023-08-09 21:19:45 -1200

    first commit

--- Operator.java
+++ Operator.java
@@ -1,82 +1,82 @@
 public class Operator{
     public static void main(String args[]){
-    
-    int a = 7;
-    int b = 23;
-    int c = 8;
-    boolean x = true;
-    boolean y = false;
+        
+        int a = 7;
+        int b = 23;
+        int c = 8;
+        boolean x = true;
+        boolean y = false;
 
-    // Arithmetic operator
-    // +,-,*,/,%
+        // Arithmetic operator
+        // +,-,*,/,%
 
-    System.out.println(a+b);
-    System.out.println(a-b);
-    System.out.println(a*b);
-    System.out.println(a/b);
-    System.out.println(a%b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
 
-    // Assignment operator
-    // =,+=,-=,*=,/=,%=,&=,|=,^=,<<=,>>=
+        // Assignment operator
+        // =,+=,-=,*=,/=,%=,&=,|=,^=,<<=,>>=
 
-    System.out.println(a += 5);
-    System.out.println(a -= 5);
-    System.out.println(a *= 5);
-    System.out.println(a /= 5);
-    System.out.println(a %= 5);
-    System.out.println(a &= 7);
-    System.out.println(a |= 5);
-    System.out.println(a ^= 6);
-    System.out.println(a <<= 1);
-    System.out.println(a >>= 2);
+        System.out.println(a += 5);
+        System.out.println(a -= 5);
+        System.out.println(a *= 5);
+        System.out.println(a /= 5);
+        System.out.println(a %= 5);
+        System.out.println(a &= 7);
+        System.out.println(a |= 5);
+        System.out.println(a ^= 6);
+        System.out.println(a <<= 1);
+        System.out.println(a >>= 2);
 
-    // Relational Operator
-    // ==,>,<,>=,<=,!=
+        // Relational Operator
+        // ==,>,<,>=,<=,!=
 
-    System.out.println(a == 5);
-    System.out.println(a < 5);
-    System.out.println(a > 5);
-    System.out.println(a >= 5);
-    System.out.println(a <= 5);
-    System.out.println(a != 5);
+        System.out.println(a == 5);
+        System.out.println(a < 5);
+        System.out.println(a > 5);
+        System.out.println(a >= 5);
+        System.out.println(a <= 5);
+        System.out.println(a != 5);
 
-    // Logical Operator
-    // &&,||,!
+        // Logical Operator
+        // &&,||,!
 
-    System.out.println(x && y);
-    System.out.println(x || y);
-    System.out.println(!x);
+        System.out.println(x && y);
+        System.out.println(x || y);
+        System.out.println(!x);
 
-    // Bitwise Operator 
-    // // &,|,^,<<,>>,~
+        // Bitwise Operator 
+        // // &,|,^,<<,>>,~
 
-    System.out.println(a & b);
-    System.out.println(a | b);
-    System.out.println(a ^ b);
-    System.out.println(a << b);
-    System.out.println(a >> b);
-    System.out.println(~a);
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(a << b);
+        System.out.println(a >> b);
+        System.out.println(~a);
 
-    //Unary Operator
-    // +,-
+        //Unary Operator
+        // +,-
 
-    System.out.println(+b);
-    System.out.println(-b);
+        System.out.println(+b);
+        System.out.println(-b);
 
-    // Trenary Operator
-    // condition ? true : false
+        // Trenary Operator
+        // condition ? true : false
 
-    int result = a>b ? a>c ?a:c:b>c ? b:c ;
-    System.out.println(result);
+        int result = a>b ? a>c ?a:c:b>c ? b:c ;
+        System.out.println(result);
 
 
-    // Increament and decreament Opeartor 
-    // ++,--
+        // Increament and decreament Opeartor 
+        // ++,--
 
-    System.out.println(++a);
-    System.out.println(--a);
-    System.out.println(a++);
-    System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a++);
+        System.out.println(a--);
     
 
     }
---
+++ StrPractice.java
@@ -0,0 +1,24 @@
+public class StrPractice {
+    public static void main(String args[]){
+
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(txt.length());  //Find the length of the string
+        System.out.println(txt.toLowerCase()); //Convert lowercase
+        System.out.println(txt.toUpperCase()); //convert uppercase
+        System.out.println(txt.indexOf("Y")); //index (the position) of the first occurrence of a specified text in a string
+
+        // String Concatenation
+        String firstName = "Siva";
+        String lastName = "kumar";
+        System.out.println(firstName+lastName);
+        System.out.println(firstName.concat(lastName));
+
+        String x ="10";
+        int y = 20;
+        System.out.println(y+x);
+
+    }
+}
