e5e6d1e61db35c4b83fb06a28d720b5c41d98612
Dharmarj-J <dharmaraj200303@gmail.com>
2023-08-09 22:37:36 -1200

    first commit

---
+++ IfCondtion.java
@@ -0,0 +1,17 @@
+public class IfCondtion{
+    public static void main(String args[]){
+
+        if(20>19)
+            System.out.println("20 is grater then 18");
+
+
+        int time = 6;
+        if (time < 10){
+            System.out.println("Good Morning.");    
+        }   else if (time < 18) {
+            System.out.println("Good day.");
+        }   else {
+            System.out.println("Good evening.");
+        }
+    }
+}
