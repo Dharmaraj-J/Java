c7f247ee5f7d1dcb7688915d1e62bf4f08c1a954
Dharmarj-J <dharmaraj200303@gmail.com>
2023-09-12 20:24:41 -1200

    first commit

---
+++ WrapperClass.java
@@ -0,0 +1,18 @@
+public class WrapperClass {
+    public static void main(String[] args){
+
+        int num = 7;
+        Integer num1 = num;  //Auto-Boxing
+
+        int num2 = num1;  //Auto-Unboxing
+
+        System.out.println(num2);
+
+        String str = "12";
+
+        int num3 = Integer.parseInt(str);
+
+         System.out.println(num3*2);
+
+    }
+}
