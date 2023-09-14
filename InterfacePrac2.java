ea957d0c5b139534b79b3d7aa9a5412cedbbcdeb
Dharmarj-J <dharmaraj200303@gmail.com>
2023-09-13 22:33:20 -1200

    updated

--- InterfacePrac2.java
+++ InterfacePrac2.java
@@ -1,6 +1,5 @@
 interface A{
 
-
     int age = 44;
     String area = "Namakkal"; //Interface attributes are by default public, static and final
 
@@ -10,12 +9,24 @@ interface A{
 }
 
 
-class B implements A{
+interface X{
+    void run();
+}
+
+interface Y extends X{
+   
+}
+
+
+class B implements A,Y{
     public void show(){
         System.out.println("in show");
     }
     public void config(){
          System.out.println("in config");
+    }
+      public void run(){
+         System.out.println("running..");
     } 
 }
 
@@ -26,7 +37,11 @@ public class InterfacePrac2 {
         obj = new B();
         obj.config();
         obj.show();
+        
 
+        X obj1 = new B();
+        obj1.run();
+        
         // A.age = 23;  It gives an error beacuse final attribute cannot be changed
         System.out.println(A.age);
         System.out.println(A.area);
