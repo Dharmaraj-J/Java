510b94332e3b7e5c28b56ee6e4836170163c60ae
Dharmarj-J <dharmaraj200303@gmail.com>
2024-01-28 10:57:52 -1200

    update

--- ExcepHand.java
+++ ExcepHand.java
@@ -1,14 +1,29 @@
 public class ExcepHand {
     public static void main(String[] args){
 
-        int n = 0;
+        int n = 2;
         int m = 0;
+
+        int[] nums = new int[5];
+        String str = null;
 
         try{
             m = 24/n;
+            System.out.println(str.length());
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
         }
-        catch(Exception e){
-            System.out.println("Something went wrong...");
+
+        catch(ArithmeticException e){
+            System.out.println("Cannot divide by zero");
+        }
+
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Stay in your limit");
+        }
+
+        catch(Exception e){   //parent class of all Exception
+            System.out.println("Something went wrong..." + e);
         }
 
         System.out.println(m);
