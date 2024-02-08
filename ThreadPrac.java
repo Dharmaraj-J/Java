3484a3cb63bc252321d4fd23805c5bb3fabaa563
Dharmarj-J <dharmaraj200303@gmail.com>
2024-02-07 22:57:56 -1200

    updated

--- ThreadPrac.java
+++ ThreadPrac.java
@@ -2,6 +2,13 @@ class Th1 extends Thread{
     public void run(){
         for(int i=0;i<=10;i++){
             System.out.println("Hi");
+            try{
+                Thread.sleep(1);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            
         }
     }
 }
@@ -10,6 +17,12 @@ class Th2 extends Thread{
     public void run(){
         for(int i=0;i<=10;i++){
             System.out.println("Hello");
+            try{
+                Thread.sleep(1);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -19,6 +32,10 @@ public class ThreadPrac {
 
         Th1 obj1 = new Th1();
         Th2 obj2 = new Th2();
+
+        // System.out.println(obj1.getPriority());  used to get the prority of thread
+
+        // obj1.setPriority(7);    used to set the priority to thread(1 to 10)
 
         obj1.start();
         obj2.start();
